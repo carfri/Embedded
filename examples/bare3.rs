ce368b72479c8b6f2f6e05cd8cc076a29c2008bf
carfri <carl.frisenstam@gmail.com>
2019-03-07 17:21:12 +0100

    bare3_4

--- examples/bare3.rs
+++ examples/bare3.rs
@@ -31,16 +31,21 @@ fn main() -> ! {
         hprint!("{},", _c).unwrap();
     }
 
+    let a: [u8; 4] = [65u8; 4];
+    let mut a = [0u8; 4];
+
+
     hprintln!("iterate iterate using (raw) indexing").unwrap();
     for _i in 0..s.len() {
         hprintln!("{},", bs[_i]).unwrap();
+        a[_i] = bs[_i];
     }
 
     hprintln!("").unwrap();
 
-    let a: [u8; 4] = [65u8; 4];
-    let mut a = [0u8; 4];
-    a.clone_from_slice(&bs[..]);
+    //let a: [u8; 4] = [65u8; 4];
+    //let mut a = [0u8; 4];
+    //a.clone_from_slice(&bs[..]);
 
     hprintln!("").unwrap();
     hprintln!("a = {}", core::str::from_utf8(&a).unwrap()).unwrap();
@@ -75,7 +80,7 @@ fn main() -> ! {
 //
 //    What is the type of `bs`?
 //
-//    it is a byte array &[bs]
+//    it is a byte array &[u8]
 //
 //    What is the type of `c`?
 //
